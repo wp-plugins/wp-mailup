ec5df3f90829298012bd187922effac7a43e1ef8
mailup <mailup@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-11 08:55:19 +0000

    fixed issues related to confirmation option and ext fields. Ready for release 1.1.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-mailup/trunk@570524 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-mailup.php
+++ wp-mailup.php
@@ -6,7 +6,7 @@
 	Plugin name: WP-MailUp
 	Plugin URI: http://wordpress.org/extend/plugins/wp-mailup/
 	Description: Wordpress plugin for MailUp Newsletter Service.
-	Version: 1.1.2
+	Version: 1.1.3
 	Author: N. I. Biz Soft Solutions
 	Author URI: http://www.nibizsoft.com/
 	License: GPLv2

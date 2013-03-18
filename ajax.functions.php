25a52667efb6dcb91f0449c8665ec00589724bf7
mailup <mailup@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-18 10:07:59 +0000

    Security vulnerability has been patched by calling is_user_logged_in() and current_user_can instead of using cookies. Ready for 2nd review
    git-svn-id: https://plugins.svn.wordpress.org/wp-mailup/trunk@683582 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ajax.functions.php
+++ ajax.functions.php
@@ -5,17 +5,19 @@
  */
  
 // Remove the two lines below when debugging
-// ini_set('display_errors',1); 
-// error_reporting(E_ALL);
+ini_set('display_errors',1); 
+error_reporting(E_ALL);
  
 $logged_in = false;
-include_once "myPluggable.php";
+//include_once "myPluggable.php";
 
 
+//global $current_user;
+//get_currentuserinfo();
 
 //if ( current_user_can( 'manage_options' ) ) /* Not used because this message is returned: Fatal error: Call to undefined function wp_get_current_user() in /usr/local/www/vhosts/wpress.xrider.it/httpdocs/wp-includes/capabilities.php on line 1281 */
 
-if( is_user_logged_in() ) 
+//if( is_user_logged_in() ) 
 {
 	$logged_in = true;
 		
@@ -34,12 +36,12 @@ if( is_user_logged_in() )
 
 if(@$_REQUEST['formData'] == 'save')
 {
-	if ($logged_in) {
+	//if ($logged_in) {
 		add_action('init', 'wpmailup_save_config');
-	}
-	else {
+	/*}
+	//else {
 		echo 'ACCESS DENIED (1)';
-	}
+	}*/
 }
 function wpmailup_save_config()
 {
@@ -48,89 +50,104 @@ function wpmailup_save_config()
 	global $wpmailup;
 	
 	//echo 'alert(' . print_r($_REQUEST, true).')';
-	
-	if(@$_REQUEST['formData'] == 'save')
+	if( is_user_logged_in() )
 	{
-		if(trim($_REQUEST['console-host']) == '')
+		if ( current_user_can( 'edit_plugins' ) )
 		{
-			//return new WP_Error('', __("I've fallen and can't get up"));
-		}
-		$wpmailup['consoleHost'] = $_REQUEST['console-host'];
-		$wpmailup['subscribePath'] = $_REQUEST['subscribe-path'];
-		$wpmailup['pluginTitle'] = $_REQUEST['plugin-title'];
-		$wpmailup['pluginDescription'] = $_REQUEST['plugin-description'];
-		/* $wpmailup['externalCss'] = $_REQUEST['external-css']; */
-		$wpmailup['cssCombination'] = $_REQUEST['css-combination'];
-		$wpmailup['submitButton'] = $_REQUEST['submit-button'];
-		$wpmailup['requestConfirm'] = $_REQUEST['request-confirm'];
-		$wpmailup['listId'] = $_REQUEST['list-id'];
-		$wpmailup['listDisplayedName'] = $_REQUEST['list-displayed-name'];
-		$wpmailup['groupId'] = $_REQUEST['group-id'];
-		
-		$wpmailup['emailShow'] = $_REQUEST['email-show'];
-		$wpmailup['emailRequired'] = $_REQUEST['email-required'];
-		/* as email fieldcode always will be Email */
-		/* $wpmailup['emailFieldcode'] = $_REQUEST['email-fieldcode']; */
-		$wpmailup['emailDisplayedName'] = $_REQUEST['email-displayed-name'];
-		
-		$wpmailup['extfield1Show'] = $_REQUEST['extfield1-show'];
-		$wpmailup['extfield1Required'] = $_REQUEST['extfield1-required'];
-		$wpmailup['extfield1Fieldcode'] = $_REQUEST['extfield1-fieldcode'];
-		$wpmailup['extfield1DisplayedName'] = $_REQUEST['extfield1-displayed-name'];
-		
-		$wpmailup['extfield2Show'] = $_REQUEST['extfield2-show'];
-		$wpmailup['extfield2Required'] = $_REQUEST['extfield2-required'];
-		$wpmailup['extfield2Fieldcode'] = $_REQUEST['extfield2-fieldcode'];
-		$wpmailup['extfield2DisplayedName'] = $_REQUEST['extfield2-displayed-name'];
-
-		$wpmailup['extfield3Show'] = $_REQUEST['extfield3-show'];
-		$wpmailup['extfield3Required'] = $_REQUEST['extfield3-required'];
-		$wpmailup['extfield3Fieldcode'] = $_REQUEST['extfield3-fieldcode'];
-		$wpmailup['extfield3DisplayedName'] = $_REQUEST['extfield3-displayed-name'];
-
-		$wpmailup['extfield4Show'] = $_REQUEST['extfield4-show'];
-		$wpmailup['extfield4Required'] = $_REQUEST['extfield4-required'];
-		$wpmailup['extfield4Fieldcode'] = $_REQUEST['extfield4-fieldcode'];
-		$wpmailup['extfield4DisplayedName'] = $_REQUEST['extfield4-displayed-name'];
-
-		$wpmailup['extfield5Show'] = $_REQUEST['extfield5-show'];
-		$wpmailup['extfield5Required'] = $_REQUEST['extfield5-required'];
-		$wpmailup['extfield5Fieldcode'] = $_REQUEST['extfield5-fieldcode'];
-		$wpmailup['extfield5DisplayedName'] = $_REQUEST['extfield5-displayed-name'];
-		
-		$wpmailup['mobileShow'] = $_REQUEST['mobile-show'];
-		$wpmailup['mobileRequired'] = $_REQUEST['mobile-required'];
-		$wpmailup['mobileFieldcode'] = $_REQUEST['mobile-fieldcode'];
-		$wpmailup['mobileDisplayedName'] = $_REQUEST['mobile-displayed-name'];
-		
-		$wpmailup['successMessage'] = stripslashes($_REQUEST['success-message']);
-		$wpmailup['genericError'] = stripslashes($_REQUEST['generic-error']);
-		$wpmailup['invalidAddress'] = stripslashes($_REQUEST['invalid-address']);
-		$wpmailup['invalidPhone'] = stripslashes($_REQUEST['invalid-phone']);
-		$wpmailup['alreadyPresent'] = stripslashes($_REQUEST['already-present']);
-		$wpmailup['fieldRequired'] = stripslashes($_REQUEST['field-required']);
-		$wpmailup['termsNotAgreed'] = stripslashes($_REQUEST['terms-not-agreed']);
-		$wpmailup['termsConfirm'] = stripslashes($_REQUEST['terms-confirm']);		
-		$tmp = $_REQUEST[('terms-n-con')];
-		$tmp=stripslashes($tmp); // see MailUp ticket 091-1874209D-0204
-		$wpmailup['termsNcon']= $tmp;	
-		
-		
-		$wpmailup['acceptanceMsg'] = $_REQUEST['acceptance-msg'];
+			if(@$_REQUEST['formData'] == 'save')
+			{
+				if(trim($_REQUEST['console-host']) == '')
+				{
+					//return new WP_Error('', __("I've fallen and can't get up"));
+				}
+				$wpmailup['consoleHost'] = $_REQUEST['console-host'];
+				$wpmailup['subscribePath'] = $_REQUEST['subscribe-path'];
+				$wpmailup['pluginTitle'] = $_REQUEST['plugin-title'];
+				$wpmailup['pluginDescription'] = $_REQUEST['plugin-description'];
+				/* $wpmailup['externalCss'] = $_REQUEST['external-css']; */
+				$wpmailup['cssCombination'] = $_REQUEST['css-combination'];
+				$wpmailup['submitButton'] = $_REQUEST['submit-button'];
+				$wpmailup['requestConfirm'] = $_REQUEST['request-confirm'];
+				$wpmailup['listId'] = $_REQUEST['list-id'];
+				$wpmailup['listDisplayedName'] = $_REQUEST['list-displayed-name'];
+				$wpmailup['groupId'] = $_REQUEST['group-id'];
 				
-		update_option('wpmailup', serialize($wpmailup));
+				$wpmailup['emailShow'] = $_REQUEST['email-show'];
+				$wpmailup['emailRequired'] = $_REQUEST['email-required'];
+				/* as email fieldcode always will be Email */
+				/* $wpmailup['emailFieldcode'] = $_REQUEST['email-fieldcode']; */
+				$wpmailup['emailDisplayedName'] = $_REQUEST['email-displayed-name'];
+				
+				$wpmailup['extfield1Show'] = $_REQUEST['extfield1-show'];
+				$wpmailup['extfield1Required'] = $_REQUEST['extfield1-required'];
+				$wpmailup['extfield1Fieldcode'] = $_REQUEST['extfield1-fieldcode'];
+				$wpmailup['extfield1DisplayedName'] = $_REQUEST['extfield1-displayed-name'];
+				
+				$wpmailup['extfield2Show'] = $_REQUEST['extfield2-show'];
+				$wpmailup['extfield2Required'] = $_REQUEST['extfield2-required'];
+				$wpmailup['extfield2Fieldcode'] = $_REQUEST['extfield2-fieldcode'];
+				$wpmailup['extfield2DisplayedName'] = $_REQUEST['extfield2-displayed-name'];
+
+				$wpmailup['extfield3Show'] = $_REQUEST['extfield3-show'];
+				$wpmailup['extfield3Required'] = $_REQUEST['extfield3-required'];
+				$wpmailup['extfield3Fieldcode'] = $_REQUEST['extfield3-fieldcode'];
+				$wpmailup['extfield3DisplayedName'] = $_REQUEST['extfield3-displayed-name'];
+
+				$wpmailup['extfield4Show'] = $_REQUEST['extfield4-show'];
+				$wpmailup['extfield4Required'] = $_REQUEST['extfield4-required'];
+				$wpmailup['extfield4Fieldcode'] = $_REQUEST['extfield4-fieldcode'];
+				$wpmailup['extfield4DisplayedName'] = $_REQUEST['extfield4-displayed-name'];
+
+				$wpmailup['extfield5Show'] = $_REQUEST['extfield5-show'];
+				$wpmailup['extfield5Required'] = $_REQUEST['extfield5-required'];
+				$wpmailup['extfield5Fieldcode'] = $_REQUEST['extfield5-fieldcode'];
+				$wpmailup['extfield5DisplayedName'] = $_REQUEST['extfield5-displayed-name'];
+				
+				$wpmailup['mobileShow'] = $_REQUEST['mobile-show'];
+				$wpmailup['mobileRequired'] = $_REQUEST['mobile-required'];
+				$wpmailup['mobileFieldcode'] = $_REQUEST['mobile-fieldcode'];
+				$wpmailup['mobileDisplayedName'] = $_REQUEST['mobile-displayed-name'];
+				
+				$wpmailup['successMessage'] = stripslashes($_REQUEST['success-message']);
+				$wpmailup['genericError'] = stripslashes($_REQUEST['generic-error']);
+				$wpmailup['invalidAddress'] = stripslashes($_REQUEST['invalid-address']);
+				$wpmailup['invalidPhone'] = stripslashes($_REQUEST['invalid-phone']);
+				$wpmailup['alreadyPresent'] = stripslashes($_REQUEST['already-present']);
+				$wpmailup['fieldRequired'] = stripslashes($_REQUEST['field-required']);
+				$wpmailup['termsNotAgreed'] = stripslashes($_REQUEST['terms-not-agreed']);
+				$wpmailup['termsConfirm'] = stripslashes($_REQUEST['terms-confirm']);		
+				$tmp = $_REQUEST[('terms-n-con')];
+				$tmp=stripslashes($tmp); // see MailUp ticket 091-1874209D-0204
+				$wpmailup['termsNcon']= $tmp;	
+				
+				
+				$wpmailup['acceptanceMsg'] = $_REQUEST['acceptance-msg'];
+						
+				update_option('wpmailup', serialize($wpmailup));
+			}
+			exit();
+		}
+		else {
+			echo 'ACCESS DENIED. USER IS NOT AUTHORIZED';
+			exit();
+		}
 	}
-	exit();
+	else {
+		echo 'ACCESS DENIED. USER SHALL BE LOGGED IN';
+		exit();
+	}
+	
 }
 
 if(@$_REQUEST['formData'] == 'load')
 {		
-	if($logged_in){
+	//if( is_user_logged_in() )
+	{
 		add_action('init', 'wpmailup_load_config');
 	}
-	else {
-		echo 'ACCESS DENIED (2)';
-	}
+	/*else {
+		echo 'ACCESS DENIED';
+	}*/
 	
 }
 
@@ -139,8 +156,14 @@ if(@$_REQUEST['formData'] == 'load')
 function wpmailup_load_config()
 {
 	global $wpmailup;
-	$wpmailup = unserialize(get_option('wpmailup'));
-	echo json_encode($wpmailup);
+	if( is_user_logged_in() )
+	{
+		if ( current_user_can( 'edit_plugins' ) )
+		{
+			$wpmailup = unserialize(get_option('wpmailup'));
+			echo json_encode($wpmailup);
+		}
+	}
 	exit();
 }
 
